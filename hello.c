464cbc2f1c4b0af750072914a5bbcb9318b3ae81
srivallioguri <86719087+srivallioguri@users.noreply.github.com>
2024-02-05 12:19:00 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,6 +4,6 @@ int main()
 {
     printf("Hello World  from Hello.c  ...");
     printf("Hello World  from Job 2  ......");
-    printf("Hello World  from Job 2 to trigger job   ......")
+    printf("Hello World  from Job 2 to trigger job   ......");
     return 0;
 }
